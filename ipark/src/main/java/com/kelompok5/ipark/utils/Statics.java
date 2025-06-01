26768da5ab18bfd86f04373f160a458653382b29
ibnucoding2423 <oniichanterritory@gmail.com>
2025-06-01 20:48:26 +0800

    move db to app

--- ipark/src/main/java/com/kelompok5/ipark/utils/Statics.java
+++ ipark/src/main/java/com/kelompok5/ipark/utils/Statics.java
@@ -1,5 +1,5 @@
 package com.kelompok5.ipark.utils;
 
 public class Statics {
-    public static String jdbcUrl = "jdbc:sqlite:/D:\\Program Files\\SQLITE\\sqlite-tools-win-x64-3490200\\iparkdb.db";
+    public static String jdbcUrl = "jdbc:sqlite:src\\main\\resources\\com\\kelompok5\\ipark\\iparkdb.db";
 }
